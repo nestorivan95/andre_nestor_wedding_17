3aa1028ab9f046dcf8b518628e1a499e1e634213
Ramirez Nestor <nestorivan00@gmail.com>
2025-12-06 16:08:15 -0500

    remove old index

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Si tu repositorio tiene un nombre diferente, ajusta el basePath
+  // basePath: '/andre_nestor_wedding',
+  // trailingSlash: true,
 }
 
 module.exports = nextConfig
